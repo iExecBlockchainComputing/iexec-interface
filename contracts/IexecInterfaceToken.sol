86e7e5319eaaa60fb42f6ff9a3abdceab07aef3b
Hadrien Croubois <hc@iex.ec>
2020-05-05 09:25:23 +0200

    version

---
+++ contracts/IexecInterfaceToken.sol
@@ -0,0 +1,28 @@
+pragma solidity >0.5.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@iexec/poco/contracts/IexecInterfaceToken.sol";
+
+
+contract IexecInterfaceToken
+{
+	address constant IEXECPROXY = 0x3eca1B216A7DF1C7689aEb259fFB83ADFB894E7f;
+
+	IexecInterfaceToken public iexecproxy;
+
+	constructor(address _iexecproxy)
+	public
+	{
+		if      (_isContract(_iexecproxy)) { iexecproxy = IexecInterfaceToken(payable(_iexecproxy)); }
+		else if (_isContract(IEXECPROXY )) { iexecproxy = IexecInterfaceToken(payable(IEXECPROXY )); }
+		else                               { revert("invalid-iexecproxy-address");                   }
+	}
+
+	function _isContract(address _addr)
+	internal view returns (bool)
+	{
+		uint32 size;
+		assembly { size := extcodesize(_addr) }
+		return size > 0;
+	}
+}
